04eb7851122770c383a976f2f68933e03f1ee90c
Xiaokai Wang <xiw69@pitt.edu>
2016-09-06 16:06:38 -0400

    Executed Exercise 3 from Chapter 1.

---
+++ Ch1-Ex3/ch1-ex3.py
@@ -0,0 +1,15 @@
+#
+# Template for code submission
+# name : Xiaokai Wang
+# email : xiw69@pitt.edu
+# date : 09/06/2016
+# class : CS0008-f2016
+# instructor : Max Novelli (man8@pitt.edu)
+#
+# Description:
+# description of this file goes here
+# Example: Starting with Python, Chapter 1, Exercise 3
+#
+# Notes:
+# any notes to the instructor and/or TA goes here
+# ...and now let's program with Python
